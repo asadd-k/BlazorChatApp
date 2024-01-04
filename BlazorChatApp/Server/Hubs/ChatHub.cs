f66ec6c101adb81e674ed3733093feff449a1afd
Asad Kherro <asadshahid958@gmail.com>
2024-01-05 01:51:25 +0500

    Add project files.

---
+++ BlazorChatApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await SendMessage(string.Empty, $"{username} Joined.");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await SendMessage(string.Empty, $"{username} left!");
+        }
+
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("GetMessage",user, message);
+        }
+    }
+}
